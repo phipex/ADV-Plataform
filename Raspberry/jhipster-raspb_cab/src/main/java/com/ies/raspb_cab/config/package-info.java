a59e09e574204f9f149513e6be6c2b06a9427a9b
betancur343ies <betancur343.ies@gmail.com>
2017-07-24 17:58:47 -0500

    feature(Raspberry): Se añade proyecto JHipster (backend) de cabina de recargas.

---
+++ Raspberry/jhipster-raspb_cab/src/main/java/com/ies/raspb_cab/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.ies.raspb_cab.config;
---
+++ Raspberry/jhipster-raspb_cab/src/main/java/com/ies/raspb_cab/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.ies.raspb_cab.repository;
+
+import com.ies.raspb_cab.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
