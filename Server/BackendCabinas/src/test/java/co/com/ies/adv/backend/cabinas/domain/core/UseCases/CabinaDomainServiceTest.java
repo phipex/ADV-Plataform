e335d5260c943c98d963ac1f9ae4752f7cb66f41
phipex <phipex@yahoo.com>
2017-07-27 19:17:58 -0500

    test(backend_cabina): se agregaron test de validacion de cabina

---
+++ Server/BackendCabinas/src/test/java/co/com/ies/adv/backend/cabinas/domain/core/UseCases/CabinaDomainServiceTest.java
@@ -0,0 +1,114 @@
+package co.com.ies.adv.backend.cabinas.domain.core.UseCases;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import co.com.ies.adv.backend.cabinas.domain.Cabina;
+import co.com.ies.adv.backend.cabinas.domain.core.entities.ICabina;
+import co.com.ies.adv.backend.cabinas.domain.core.enumeration.EstadoCabina;
+import co.com.ies.adv.backend.cabinas.domain.core.exceptions.CabinaException;
+import co.com.ies.adv.backend.cabinas.domain.core.repositorys.ICabinaRepository;
+
+public class CabinaDomainServiceTest {
+
+	@Rule
+    public ExpectedException exceptions = ExpectedException.none();
+	
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void testValidaNoExisteCabinaAsociada() throws CabinaException{
+		MockRepository mockRepository = new MockRepository();
+		
+		exceptions.expect(CabinaException.class);
+        exceptions.expectMessage(CabinaDomainService.NO_HAY_CABINA_ASOCIADA_AL_USUARIO);
+		
+		CabinaDomainService service = new CabinaDomainService();
+		
+		service.setICabinaRepository(mockRepository);
+		
+		service.validaCabina(0L);
+		
+	}
+	
+	@Test
+	public void testValidaCabinaInactiva() throws CabinaException{
+		MockRepository mockRepository = new MockRepository();
+		
+		exceptions.expect(CabinaException.class);
+        exceptions.expectMessage(CabinaDomainService.CABINA_SE_ENCUENTRA_INACTIVA);
+		
+		CabinaDomainService service = new CabinaDomainService();
+		Cabina cabina = new Cabina();
+		cabina.setEstado(EstadoCabina.INACTIVO);
+		
+		mockRepository.setICabinaMock(cabina);
+		
+		
+		service.setICabinaRepository(mockRepository);
+		
+		service.validaCabina(0L);
+		
+	}
+	
+	@Test
+	public void testValidaCabinaSinCupo() throws CabinaException{
+		MockRepository mockRepository = new MockRepository();
+		
+		exceptions.expect(CabinaException.class);
+        exceptions.expectMessage(CabinaDomainService.LA_CABINA_NO_TIENE_CUPO);
+		
+		CabinaDomainService service = new CabinaDomainService();
+		Cabina cabina = new Cabina();
+		cabina.setEstado(EstadoCabina.ACTIVO);
+		
+		mockRepository.setICabinaMock(cabina);
+		
+		
+		service.setICabinaRepository(mockRepository);
+		
+		service.validaCabina(0L);
+		
+	}
+	
+	public static class MockRepository implements ICabinaRepository{
+
+		ICabina cabinaMock;
+		
+		public void setICabinaMock(Cabina cabina){
+			this.cabinaMock = cabina;
+		}
+		
+		@Override
+		public ICabina findOneByUserId(Long userId) {
+			
+			return cabinaMock;
+		}
+		
+		
+		
+	}
+	
+
+}
